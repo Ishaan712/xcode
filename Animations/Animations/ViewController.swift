f02b2a18a12a006a6a7cfb64ea055a6ca633ce62
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2019-07-03 15:35:14 -0700

    animation

--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -32,7 +32,37 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    /*override func viewDidLayoutSubviews() {
+        
+        //pandaImage.center = CGPoint(x: pandaImage.center.x - 400, y: pandaImage.center.y) // slides in from the left animation
+        
+        //pandaImage.alpha = 0 // fades the sprite in animation
+     
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        UIView.animate(withDuration: 1, animations: { () -> Void in // closure so need SELF before actions
+            
+            //self.pandaImage.center = CGPoint(x: self.pandaImage.center.x + 400, y: self.pandaImage.center.y) // slides in from left animation
+            
+            //self.pandaImage.alpha = 1 // fades the sprite in animation
+            
+            self.pandaImage?.transform = CGAffineTransform(scaleX: 0.0, y: 0.0) //decrease size animation
+            
+        }){ (_ finished: Bool) in
+            
+            UIView.animate(withDuration: 1, animations: {
+                
+                self.pandaImage?.transform = CGAffineTransform(scaleX: 2, y: 2)// increase size animation
+                
+                self.pandaImage?.center = CGPoint(x: self.pandaImage.center.x - 30, y: self.pandaImage.center.y) // move image so it fits in the area
+            })
+        }
+        
+    }*/
 
 }
 
