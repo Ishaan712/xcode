eddb1aff8ba387a53c239e6f06327fba10c30011
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2019-07-11 16:37:47 -0700

    find user's locations

--- Maps/Maps/ViewController.swift
+++ Maps/Maps/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 import MapKit // Map frameworks
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+// In Build Phase by clicking on the top folder of app: add core location framework
+
+//In Info.plist which has all info about app: add NSLocationWhenInUseUsageDescription (when app is running) and NSLocationAlwaysUsageDescription (when running in the background)
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {// get user's location within view controller
 
     @IBOutlet var map: MKMapView!
     
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // Request user's location
+        
+        locationManager.delegate = self
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // how strong for accuracy of the location
+        
+        locationManager.requestWhenInUseAuthorization() // request the location only when the app is in use
+        
+        locationManager.startUpdatingLocation()
+        
+        
+        
+        // setting up the map and zooming in on a particular location
         
         let latitude:CLLocationDegrees = 37.460927
         
@@ -31,6 +53,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let region:MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
         
         map.setRegion(region, animated: true)
+        
+        
         
         // Make points on the map or annotations
         
@@ -58,24 +82,51 @@ class ViewController: UIViewController, MKMapViewDelegate {
         map.addGestureRecognizer(uilpgr)
     }
     
-    
+    // for putting a pin after a long press at a location
     @objc func userAction(gestureRecongnizer:UIGestureRecognizer) { // recieves a guesture into the function which has a type of gesturerecognizer
         
         print("Gesture recongized")
         
         let touchPoint = gestureRecongnizer.location(in: self.map) // gives the location relative to the location on the map    NOT ACTUAL POINTS IN THE WORLD
         
-        let newCoordinates:CLLocationCoordinate2D = map.convert(touchPoint, toCoordinateFrom: self.map)
+        let newCoordinates:CLLocationCoordinate2D = map.convert(touchPoint, toCoordinateFrom: self.map) // convert from points on the map to actual points in the world
         
         let annotation = MKPointAnnotation()
         
         annotation.coordinate = newCoordinates
         
-        annotation.title = "New Place"
+        annotation.title = "New Place" // put a pin with annotation on the new place
         
         annotation.subtitle = "Long Pressed Here"
         
         map.addAnnotation(annotation)
+        
+        
+    }
+    
+    
+    // for getting the user's locations
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) { // will be called when a new location is registered by the phone
+        
+        print(locations)r
+        
+        let userLocation:CLLocation = locations[0] // extracted the location fromt the variable: locations
+        
+        let latitude = userLocation.coordinate.latitude // put the latitude into a variable
+        
+        let longitude = userLocation.coordinate.longitude // put the longitude into a variable
+        
+        let latDelta:CLLocationDegrees = 0.001 // zoom in or out; 1 being zommed out and 0.00001 being zommed in
+        
+        let longDelta:CLLocationDegrees = 0.001
+        
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
         
         
     }
