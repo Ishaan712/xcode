1d1d52665963c0a3b3787d996cf69abc37e53058
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2019-07-17 21:46:25 -0700

    update

--- Places/Places/TableViewController.swift
+++ Places/Places/TableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-var places = [Dictionary<String,String>()]
+var places = [Dictionary<String,String>()] // dictionary to store locations
 
 var activePlace = -1
 
@@ -19,19 +19,14 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(places.count)
-        print(places[0])
-        
         if UserDefaults.standard.object(forKey: "places") != nil { //checks if the list is not empty
             
-            places = UserDefaults.standard.object(forKey: "places") as! [Dictionary<String, String>]
+            places = UserDefaults.standard.object(forKey: "places") as! [Dictionary<String, String>] // permanent storage
         }
         
     }
-
-    // MARK: - Table view data source
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) { // when the app appears
         
         if places.count == 1 && didLoad == false {
             
@@ -39,7 +34,7 @@ class TableViewController: UITableViewController {
             
             places.remove(at: 0)
             
-            places.append(["name":"Ex. Eiffel Tower", "lat": "48.858324", "lon": "2.294764"])
+            places.append(["name":"Ex. Eiffel Tower", "lat": "48.858324", "lon": "2.294764"]) // add this location as an exampled
 
         }
         
@@ -62,11 +57,11 @@ class TableViewController: UITableViewController {
         
     }
     
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) { // able to delete locations
         
         if editingStyle == .delete {
             
-            places.remove(at: indexPath.row)
+            places.remove(at: indexPath.row) // update the dictionary
             
             tableView.deleteRows(at: [indexPath], with: .bottom)
             
@@ -92,11 +87,11 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return places.count
+        return places.count // amount of rows
         
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { // create each cell with the information in the dictionary at each row in the table
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
@@ -104,51 +99,5 @@ class TableViewController: UITableViewController {
         
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
