48ba49b22d78d0f4c921a43feceae7ed4df94473
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2019-07-22 17:38:04 -0700

    Music

---
+++ Music/Music/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Ishaan Sathaye on 7/22/19.
+//  Copyright © 2019 NextGenTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
