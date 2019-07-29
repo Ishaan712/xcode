ff5a6bd190b2c6b628bc0106e3b183d505832c86
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2019-07-28 23:18:32 -0700

    dark mode music

---
+++ Music/Music/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Ishaan Sathaye on 7/28/19.
+//  Copyright © 2019 NextGenTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        overrideUserInterfaceStyle = .dark
+    }
+
+
+}
+
