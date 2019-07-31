6b9141ec088e457703efe6fb6a9523e118b46bc3
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2019-07-31 15:53:20 -0700

    final update
    finished blog reader using google api

--- Blogs/Blogs/MasterViewController.swift
+++ Blogs/Blogs/MasterViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 import CoreData
 
 class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
-
-
+    
+    var managedObjectContext:NSManagedObjectContext? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let appDel:AppDelegate = UIApplication.shared.delegate as! AppDelegate
-                
+        
         let context:NSManagedObjectContext = appDel.persistentContainer.viewContext
         
         let url = NSURL(string: "https://www.googleapis.com/blogger/v3/blogs/10861780/posts?key=AIzaSyBavZRVigpFblBI_nYaaPuVpv5N_nQQhlw")
@@ -43,16 +44,16 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
                         
                         if let items = jsonResult["items"] as? NSArray {
                             
-                            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Posts")
+                            let fetchRequest1 = NSFetchRequest<NSFetchRequestResult>(entityName: "Posts")
                             
-                            fetchRequest.returnsObjectsAsFaults = false
+                            fetchRequest1.returnsObjectsAsFaults = false
                             
                             do {
                                 
-                                var results = try context.fetch(fetchRequest)
+                                let results = try context.fetch(fetchRequest1)
                                 
                                 if results.count > 0 {
-                                
+                                    
                                     for result in results {
                                         
                                         context.delete(result as! NSManagedObject)
@@ -73,7 +74,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
                             }
                             
                             for item in (items as? [[String:Any]])! {
-                            
+                                
                                 if let title = item["title"] as? String {
                                     
                                     if let content = item["content"] as? String  {
@@ -113,42 +114,97 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         task.resume()
         
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
-
+    
     // MARK: - Segues
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
-        
-            print("Show Detail")
+            
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let object = fetchedResultsController.object(at: indexPath)
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+            }
             
         }
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        let sectionInfo = fetchedResultsController.sections![section]
+        return sectionInfo.numberOfObjects
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel!.text = "Test"
+        
+        configureCell(cell, atIndexPath: indexPath as NSIndexPath)
+        
         return cell
     }
-
+    
+    func configureCell(_ cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
+        
+        let object = self.fetchedResultsController.object(at: indexPath as IndexPath) as! NSManagedObject
+        
+        cell.textLabel!.text = (object.value(forKey: "title")! as AnyObject).description
+    }
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return false
     }
-
+    
+    
+    var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult> {
+        if _fetchedResultsController != nil {
+            return _fetchedResultsController!
+        }
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>()
+        let entity = NSEntityDescription.entity(forEntityName: "Posts", in: self.managedObjectContext!)
+        fetchRequest.entity = entity
+        
+        // Set the batch size to a suitable number.
+        fetchRequest.fetchBatchSize = 20
+        
+        // Edit the sort key as appropriate.
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: false)
+        
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // Edit the section name key path and cache name if appropriate.
+        // nil for section name key path means "no sections".
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
+        aFetchedResultsController.delegate = self
+        _fetchedResultsController = aFetchedResultsController
+        
+        do {
+            try _fetchedResultsController!.performFetch()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+        
+        return _fetchedResultsController!
+    }
+    var _fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>? = nil
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
 }
 
