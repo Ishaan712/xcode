a8a66a58b6491cf515ec56f33bc19b4369f81085
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2019-11-06 15:22:28 -0800

    update

--- How Many Fingers??/How Many Fingers??/ViewController.swift
+++ How Many Fingers??/How Many Fingers??/ViewController.swift
@@ -30,6 +30,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
 
 
 }
